eaf04289d9d95a0447cd00aa75bd61f4b00faae3
Julio <julio.i.espinosa@gmail.com>
2018-09-27 07:23:19 -0500

    Played around with matplotlib and pandas

--- read_write/read_csv_pandas.py
+++ read_write/read_csv_pandas.py
@@ -1,5 +1,16 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-df = pandas.read_csv("random.csv")
 
-print(df)
+#df = pandas.read_csv("nomames.txt")
+
+#print(df)
+
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+
+ts = ts.cumsum()
+
+ts.plot()
+
+plt.show()
